6df736e6b6a7830d6ec365669b75825b4577d7f3
rebeccastarr21 <55964395+rebeccastarr21@users.noreply.github.com>
2021-05-11 07:11:27 -0700

    Update database_queries_rs.sql

--- database_queries_rs.sql
+++ database_queries_rs.sql
@@ -1,11 +1,11 @@
 -- Shelters
 -- get all Shelter info to populate the Shelters table
-SELECT shelterID, registrationDate, name, streetAddress, city, state, phoneNumber, fax, email, sponsorable FROM shelters
+SELECT * FROM shelters;
 
 -- Pets
 -- get all Shelter names and IDs to populate the shelter dropdown
-SELECT name, shelterID FROM shelters
+SELECT name, shelterID FROM shelters;
 
 -- get all Pet info to populate the Pets table
-SELECT pets.petID, pets.registrationDate, pets.name, pets.birthday, pets.animal, pets.breed, pets.personality, pets.adoptable, pets.goal, pets.shelterID, shelters.name FROM pets
-LEFT JOIN shelters ON pets.shelterID = shelters.shelterID
+SELECT pets.*, shelters.name FROM pets
+LEFT JOIN shelters ON pets.shelterID = shelters.shelterID;
