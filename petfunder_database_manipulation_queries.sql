ff3ff269eb4563be6c08820ca45dadc697a7671b
leeja6 <leeja6@oregonstate.edu>
2021-05-11 20:31:17 -0700

    changed capitalization

--- petfunder_database_manipulation_queries.sql
+++ petfunder_database_manipulation_queries.sql
@@ -1,26 +1,26 @@
 -- Shelters
 -- get all Shelter info to populate the Shelters table
-SELECT * FROM shelters;
+SELECT * FROM Shelters;
 
 -- update a shelter's data based on submission of the Edit Shelter form 
-UPDATE shelters SET name = :nameInput, streetAddress = :streetAddressInput, city = :cityInput, state = :stateInput, phoneNumber = :phoneNumberInput, fax = :faxInput, email = :emailInput, spomsorable = :sponsorableInput WHERE shelterID = :shelterID_from_the_edit_form;
+UPDATE Shelters SET name = :nameInput, streetAddress = :streetAddressInput, city = :cityInput, state = :stateInput, phoneNumber = :phoneNumberInput, fax = :faxInput, email = :emailInput, spomsorable = :sponsorableInput WHERE shelterID = :shelterID_from_the_edit_form;
 
 -- delete a shelter
-DELETE FROM shelters WHERE shelterID = :shelterID_from_delete_button
+DELETE FROM Shelters WHERE shelterID = :shelterID_from_delete_button
 
 -- Pets
 -- get all Shelter names and IDs to populate the shelter dropdown
-SELECT name, shelterID FROM shelters;
+SELECT name, shelterID FROM Shelters;
 
 -- get all Pet info to populate the Pets table
-SELECT pets.*, shelters.name FROM pets
-LEFT JOIN shelters ON pets.shelterID = shelters.shelterID;
+SELECT pets.*, shelters.name FROM Pets
+LEFT JOIN Shelters ON Pets.shelterID = Shelters.shelterID;
 
 -- update a pet's data based on submission of the Edit Pet form 
-UPDATE pets SET name = :nameInput, birthday = :birthdayInput, animal = :animalInput, breed = :breedInput, personality = :personalityInput, adoptable = :adoptableInput, goald = :goalInput, shelterID = :shelterIDInput WHERE petID = :petID_from_the_edit_form;
+UPDATE Pets SET name = :nameInput, birthday = :birthdayInput, animal = :animalInput, breed = :breedInput, personality = :personalityInput, adoptable = :adoptableInput, goald = :goalInput, shelterID = :shelterIDInput WHERE petID = :petID_from_the_edit_form;
 
 -- delete a pet
-DELETE FROM pets WHERE petID = :petID_from_delete_button
+DELETE FROM Pets WHERE petID = :petID_from_delete_button
 
 -- get all columns from pet sponsorships to populate the display for pet sponsorships
 SELECT Sponsors.sponsorID, firstName, lastName, Pets.petID, name, lastName, amount, beginDate, endDate from PetSponsorships
